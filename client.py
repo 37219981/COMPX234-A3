c623631c32a07fe4368f17bdadaccb5abe0f4a1c
37219981 <2929336132@qq.com>
2025-05-08 23:41:33 +0800

    Update client.py
    Socket Creation and Connection

--- client.py
+++ client.py
@@ -7,3 +7,7 @@ def main():
         print("Usage: python client.py <hostname> <port> <request_file>")
         sys.exit(1)
     hostname, port, file_path = sys.argv[1], int(sys.argv[2]), sys.argv[3]
+
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((hostname, port))
