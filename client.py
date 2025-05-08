1a7a7d209d90e4d734c5a2a0864d274326f2ba08
37219981 <2929336132@qq.com>
2025-05-08 23:24:20 +0800

    main

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+import sys
+
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python client.py <hostname> <port> <request_file>")
+        sys.exit(1)
+    hostname, port, file_path = sys.argv[1], int(sys.argv[2]), sys.argv[3]
